7d6186be2faf888b797fded9549437fe71d9e708
James J Balamuta <james.balamuta@gmail.com>
2017-11-05 08:40:06 -0600

    Change base settings and include missing pieces by default

--- R/latex_templates.R
+++ R/latex_templates.R
@@ -36,15 +36,20 @@ latex_journal_format = function(..., keep_tex = TRUE, citation_package = 'natbib
     ..., template = template, keep_tex = keep_tex, citation_package = citation_package
   )
 
-  base$knitr$opts_chunk$prompt = TRUE
-  base$knitr$opts_chunk$comment = NA
-  base$knitr$opts_chunk$highlight = FALSE
+  base$knitr$opts_chunk$prompt = FALSE   # changed from TRUE
+  base$knitr$opts_chunk$comment = '# '   # default to one hashmark
+  base$knitr$opts_chunk$highlight = TRUE # changed as well
 
   base$knitr$opts_chunk$dev.args = list(pointsize = 11)
   base$knitr$opts_chunk$fig.width = 4.9 # 6.125" * 0.8, as in template
   base$knitr$opts_chunk$fig.height = 3.675 # 4.9 * 3:4
   base$knitr$opts_chunk$fig.align = "center"
-  base$knitr$knit_hooks$plot = knitr::hook_plot_tex
+
+  for (template_file in c("jss.cls", "jss.bst", "stat432-logo.png"))
+    if (!file.exists(template_file))
+      file.copy(system.file("rmarkdown", "templates", "pdf", "skeleton",
+                            template_file, package="uiucthemes"),
+                ".")
 
   base
 }
