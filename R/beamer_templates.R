378d18c816945e11edcddf77d487765e13c9228a
James J Balamuta <coatless@users.noreply.github.com>
2019-09-30 15:01:04 -0500

    Illinois Beamer Theme based on Metropolis (#19)
    * Switch to markdown in roxygen2 comments
    
    * Add an export statement for metropolis
    
    * - Fix includes
    - Lower the slide level to 2 since no subsection is suported.
    
    * Add demo of beamer_imetropolis
    
    * Add beamer_imetropolis theme
    
    * Remove an extra definition
    
    * Add release note
    
    * Add details on hacking metropolis.
    
    Modify the progress bar to use orange

--- R/beamer_templates.R
+++ R/beamer_templates.R
@@ -6,7 +6,7 @@
 #' @inheritParams rmarkdown::beamer_presentation
 #'
 #' @return
-#' A modified \code{beamer_presentation} based on the "Illinois" (UIUC)
+#' A modified `beamer_presentation`  based on the "Illinois" (UIUC)
 #' Beamer themed template.
 #'
 #' @export
@@ -24,7 +24,7 @@
 #' rmarkdown::render("slide_deck/slide_deck.Rmd")
 #' }
 beamer_illinois <- function(toc = FALSE,
-                            slide_level = 3,
+                            slide_level = 2,
                             incremental = FALSE,
                             fig_width = 10,
                             fig_height = 7,
@@ -73,8 +73,9 @@ beamer_illinois <- function(toc = FALSE,
 #'
 #' @inheritParams rmarkdown::beamer_presentation
 #'
-#' @return A modified \code{beamer_presentation} based on the "Illinois" (UIUC)
-#'  Beamer themed template.
+#' @return
+#' A modified `beamer_presentation`  based on the "Illinois" (UIUC)
+#' Beamer themed template.
 #'
 #' @export
 #'
@@ -143,7 +144,7 @@ beamer_orange <- function(toc = FALSE,
 #' @inheritParams rmarkdown::beamer_presentation
 #'
 #' @return
-#' A modified \code{beamer_presentation} based on the "marketlab" (UIUC)
+#' A modified `beamer_presentation`  based on the "marketlab" (UIUC)
 #' Beamer themed template.
 #'
 #' @export
@@ -203,4 +204,77 @@ beamer_mil <- function(toc = FALSE,
                                  pandoc_args = pandoc_args)
 
 }
+
+
+#' Beamer Illinois Metropolis Template for RMarkdown
+#'
+#' Generates from an RMarkdown file a Beamer presentation with "Illinois" (UIUC)
+#' colors and identity standards based on the Metropolis theme.
+#'
+#' @inheritParams rmarkdown::beamer_presentation
+#'
+#' @return
+#' A modified `beamer_presentation` based on the "Illinois" (UIUC)
+#' Beamer themed template.
+#'
+#' @export
+#'
+#' @author
+#' James Joseph Balamuta (Theme Hooks)
+#' See AUTHORS for more specific details behind each of the contributions.
+#'
+#' @examples
+#' \dontrun{
+#' # Generate slide deck from beamer template
+#' rmarkdown::draft("slide_deck.Rmd", template = "beamer_imetropolis", package = "uiucthemes")
+#'
+#' # Compile the document
+#' rmarkdown::render("slide_deck/slide_deck.Rmd")
+#' }
+beamer_imetropolis <- function(toc = FALSE,
+                          slide_level = 3,
+                          incremental = FALSE,
+                          fig_width = 10,
+                          fig_height = 7,
+                          fig_crop = TRUE,
+                          fig_caption = TRUE,
+                          dev = 'pdf',
+                          df_print = "default",
+                          fonttheme = "default",
+                          highlight = "default",
+                          keep_tex = FALSE,
+                          latex_engine = "pdflatex",
+                          citation_package = c("none", "natbib", "biblatex"),
+                          includes = NULL,
+                          md_extensions = NULL,
+                          pandoc_args = NULL){
+
+  template <- find_resource("beamer_imetropolis", "imetropolis.tex")
+
+  #load_resources_if_missing("beamer_orange", c("ilogo.png"))
+
+  rmarkdown::beamer_presentation(#template = template,
+                                 toc = toc,
+                                 slide_level = slide_level,
+                                 incremental = incremental,
+                                 fig_width = fig_width,
+                                 fig_height = fig_height,
+                                 fig_crop = fig_crop,
+                                 fig_caption = fig_caption,
+                                 theme = "metropolis",
+                                 dev = dev,
+                                 df_print = df_print,
+                                 fonttheme = fonttheme,
+                                 highlight = highlight,
+                                 keep_tex = keep_tex,
+                                 latex_engine = latex_engine,
+                                 citation_package = citation_package,
+                                 includes = rmarkdown::includes(in_header = c(template, includes$in_header),
+                                                     before_body = includes$before_body,
+                                                     after_body = includes$after_body),
+                                 md_extensions = md_extensions,
+                                 pandoc_args = pandoc_args)
+
+}
+
 
